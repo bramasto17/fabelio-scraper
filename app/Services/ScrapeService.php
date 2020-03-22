898b4953db2e35060e057d316cb7334dafd17674
Bramasto17 <bram@umroh.com>
2020-03-22 17:15:47 +0700

    implement try catch

--- app/Services/ScrapeService.php
+++ app/Services/ScrapeService.php
@@ -6,39 +6,42 @@ class ScrapeService extends \App\Services\BaseService
 {
     public function scrape($url)
     {
-        // Parse HTML
-        $html = $this->call('GET',$url,[],null,false);
+        try{
+            // Parse HTML
+            $html = $this->call('GET',$url,[],null,false);
 
-        // Get Product Id from HTML
-        $productId = $this->getStringBetween($html, "dataProductIdSelector = '[data-product-id=", ']');
+            // Get Product Id from HTML
+            $productId = $this->getStringBetween($html, "dataProductIdSelector = '[data-product-id=", ']');
 
-        // Get Product Desc from HTML
-        $description = strip_tags($this->getStringBetween($html, "<div id=\"description\">", '</div>'));
+            // Get Product Desc from HTML
+            $description = strip_tags($this->getStringBetween($html, "<div id=\"description\">", '</div>'));
 
-        // Get Product Data (Name, Price, Image) from API
-        $productData = $this->call('GET','https://fabelio.com/insider/data/product/id/'.$productId);
-        $name = $productData['product']['name'];
-        $regular_price = $productData['product']['unit_price'];
-        $final_price = $productData['product']['unit_sale_price'];
-        $product_image_url = $productData['product']['product_image_url'];
+            // Get Product Data (Name, Price, Image) from API
+            $productData = $this->call('GET','https://fabelio.com/insider/data/product/id/'.$productId);
+            $name = $productData['product']['name'];
+            $regular_price = $productData['product']['unit_price'];
+            $final_price = $productData['product']['unit_sale_price'];
+            $product_image_url = $productData['product']['product_image_url'];
 
-        // Get Product Images from API
-        $product_gallery = [];
-        $productGallery = $this->call('GET','https://fabelio.com/swatches/ajax/media/?product_id='.$productId);
-        foreach ($productGallery['gallery'] as $key => $gallery) {
-            $product_gallery[] = $gallery['medium'];
+            // Get Product Images from API
+            $product_gallery = [];
+            $productGallery = $this->call('GET','https://fabelio.com/swatches/ajax/media/?product_id='.$productId);
+            foreach ($productGallery['gallery'] as $key => $gallery) {
+                $product_gallery[] = $gallery['medium'];
+            }
+
+            return [
+                'product_id' => $productId,
+                'name' => $name,
+                'description' => $description,
+                'regular_price' => $regular_price,
+                'final_price' => $final_price,
+                'product_image_url' => $product_image_url,
+                'product_gallery' => $product_gallery
+            ];
+        } catch (\Exception $e) {
+            abort(400, 'URL Invalid');
         }
-
-        return [
-            'product_id' => $productId,
-            'name' => $name,
-            'description' => $description,
-            'regular_price' => $regular_price,
-            'final_price' => $final_price,
-            'product_image_url' => $product_image_url,
-            'product_gallery' => $product_gallery
-        ];
-
     }
 
     protected function getStringBetween($string, $start, $end){
