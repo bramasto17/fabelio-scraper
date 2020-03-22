81e212c4f494ce7ce6393f177724c405e264c037
Bramasto17 <bram@umroh.com>
2020-03-22 18:38:25 +0700

    add soft delete

--- app/Models/PriceHistories.php
+++ app/Models/PriceHistories.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PriceHistories extends Model
 {
+    use SoftDeletes;
+    
     protected $table   = 'product_price_histories';
     protected $guarded = array('id');
 
--- app/Models/ProductGalleries.php
+++ app/Models/ProductGalleries.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductGalleries extends Model
 {
+    use SoftDeletes;
+    
     protected $table   = 'product_galleries';
     protected $guarded = array('id');
 
