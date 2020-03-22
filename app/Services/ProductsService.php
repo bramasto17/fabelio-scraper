9e0de16b435ada04d373f199515bc04a74a0f38c
Bramasto17 <bram@umroh.com>
2020-03-22 17:50:32 +0700

    insert product data to databse

--- app/Services/ProductsService.php
+++ app/Services/ProductsService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Products;
+use App\Models\ProductGalleries;
 use App\Models\PriceHistories;
 use App\Services\ScrapeService;
 
@@ -16,13 +17,57 @@ class ProductsService extends \App\Services\BaseService
     public function submit(array $data)
     {
         $data = $this->scrapeService->scrape($data['url']);
-        dd($data);
+        // dd($data);
+
+        return $this->atomic(function() use ($data) {
+            // Create Product Data
+            $product = $this->createProduct(array_except($data,['product_gallery']));
+            // dd($product);
+
+            // Create Product Gallery
+            $productGallery = $this->createProductGallery($data['product_gallery'], $product['id']);
+
+            // Create Price History
+            $priceHistory = $this->createPriceHistory($data, $product['id']);
+
+            return $product;
+        });
+
     }
 
-    public function create(array $data)
+    protected function createProduct(array $data)
     {
         return $this->atomic(function() use ($data) {
             $result = Products::create($data)->toArray();
+
+            return $result;
+        });
+    }
+
+    protected function createProductGallery(array $data, $productId)
+    {
+        return $this->atomic(function() use ($data, $productId) {
+            $results = [];
+
+            foreach ($data as $value) {
+                $value['product_id'] = $productId;
+                $results[] = ProductGalleries::create($value)->toArray();
+            }
+
+            return $results;
+        });
+    }
+
+    protected function createPriceHistory(array $data, $productId)
+    {
+        return $this->atomic(function() use ($data, $productId) {
+            $data = [
+                'product_id' => $productId,
+                'final_price' => $data['final_price'],
+                'regular_price' => $data['regular_price'],
+            ];
+
+            $result = PriceHistories::create($data)->toArray();
 
             return $result;
         });
