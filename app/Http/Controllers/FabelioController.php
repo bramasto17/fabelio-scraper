88152b29bb9c0896a7b97ea7dd996d1b3b0ec620
Bramasto17 <bram@umroh.com>
2020-03-23 11:45:02 +0700

    cleanup

--- app/Http/Controllers/FabelioController.php
+++ app/Http/Controllers/FabelioController.php
@@ -16,7 +16,6 @@ class FabelioController extends Controller
 
     public function showSubmit()
     {
-    	// dd('showSubmit');
         return view('fabelio.submit');
     }
 
@@ -37,8 +36,8 @@ class FabelioController extends Controller
     {
         $attributes = $request->all();
         $results = $this->productsService->getAll($attributes);
-
     	dd($results);
+
         return view('fabelio.products.submit');
     }
 
@@ -56,52 +55,4 @@ class FabelioController extends Controller
         
         return $result;
     }
-
-    // public function getAll(Request $request)
-    // {
-    //     $attributes = $request->all();
-    //     $results = $this->productsService->getAll($attributes);
-    //     dd($results);
-
-    //     // return view('hrms.products.list', compact('results'));
-    // }
-
-    // public function add()
-    // {
-    // 	dd('add');
-    //     return view('hrms.products.add');
-    // }
-
-    // public function create(Request $request)
-    // {
-    //     $attributes = $request->all();
-    //     $result = $this->productsService->create($attributes);
-
-    //     // \Session::flash('flash_message', 'Department successfully Created!');
-    // 	dd('create');
-    //     return redirect('products');
-    // }
-
-    // public function edit($id)
-    // {
-    //     $result = $this->productsService->getById($id);
-
-    //     return view('hrms.products.edit', compact('result'));
-    // }
-
-    // public function update(Request $request, $id){
-    //     $attributes = array_except($request->all(), ['_token']);
-    //     $result = $this->productsService->update($attributes, $id);
-
-    //     \Session::flash('flash_message', 'Department successfully Updated!');
-    //     return redirect('products');
-    // }
-
-    // public function delete($id)
-    // {
-    //     $this->productsService->delete($id);
-
-    //     \Session::flash('flash_message', 'Department successfully Deleted!');
-    //     return redirect('products');
-    // }
 }
